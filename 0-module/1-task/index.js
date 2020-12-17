febb94d8ba9ef0591d74be89379309b55c7fcda9
iokky <58838881+iokky@users.noreply.github.com>
2020-12-17 15:39:24 +0300

    done

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,17 @@
  * @param {number} n
  * @returns {number}
  */
-function sum(m, n) {
-  return m + n;
+// function sum(m, n) {
+//   return m + n;
+// }
+
+function factorial(n) {
+  while (n!= 1){
+    n = n * (n-1);
+    console.log(n);
+  }
+
 }
+
+
+factorial(5)
