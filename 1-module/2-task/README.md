9f52239dec38b435b0384d5d5f0c2ec3761f466a
iokky <58838881+iokky@users.noreply.github.com>
2020-12-18 16:03:24 +0300

    1-Module 2-Task Done

--- 1-module/2-task/README.md
+++ 1-module/2-task/README.md
@@ -16,7 +16,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  
 }
 
 /**
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,12 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+
+  if(name.length === 0 || name.length < 4 || name.includes(" ")){
+    return false
+  }else{
+    return true
+  }
 }
 
 function sayHello() {
@@ -22,3 +27,6 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+
+isValid('sdsa a')
