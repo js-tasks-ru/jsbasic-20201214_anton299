c6eba22d7456da52ff0d306cd2780cd35d62a485
iokky <58838881+iokky@users.noreply.github.com>
2021-01-08 07:07:48 +0300

    3-Module 1-Task rework

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -3,11 +3,6 @@
  * @returns {string[]}  объект
  */
 
-let vasya = { name: 'Вася', age: 25 };
-let petya = { name: 'Петя', age: 30 };
-let masha = { name: 'Маша', age: 28 };
-
-let users = [ vasya, petya, masha ];
 
 
 function namify(users) {
